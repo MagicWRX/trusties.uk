f682b14d8b77008bf6de31d5518d62b26055abae
Brian Lindahl <brian@magicwrx.com>
2026-05-17 14:33:15 +0000

    fix: vendored shared packages as file: deps for Vercel deployment

--- prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Vendored shared packages
-mkdir -p node_modules/@magicwrxtools
-for pkg in auth-tool stripe-tool theme-manager block-system; do
-  if [ -d "vendor/$pkg" ]; then
-    ln -sfn "../../vendor/$pkg" "node_modules/@magicwrxtools/$pkg"
-  fi
-done
